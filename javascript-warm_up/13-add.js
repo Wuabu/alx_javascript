159b81553c675aaac9139b2cfb719c40d7f958f2
Wuabu <wuabueric98@gmail>
2024-01-30 17:59:21 +0000

    addf

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,4 +1,20 @@
-add();
-function add (num, num1){
-    return num + num1;
-} console.log(add(num,num1));
+//add();
+//function add (num, num1){
+//    return num + num1;
+//} console.log(add(num,num1));
+
+// 13-add.js
+
+// Define and export the add function
+module.exports.add = function add(num1, num2) {
+    return num1 + num2;
+};
+
+
+// 13-main.js
+
+// Import the add function from 13-add.js
+const add = require('./13-add').add;
+
+// Call the add function with arguments 3 and 5 and print the result
+console.log(add(3, 5));
