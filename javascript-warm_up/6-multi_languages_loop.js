a188c350ab12c3aaaaf9da896cdf630f93bd2065
Wuabu <wuabueric98@gmail>
2024-01-30 07:41:20 +0000

    eeee

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,4 @@
-const lines = ["C is fun","Python is cool", "JavaScript is amazing"];
+const lines = ("C is fun","Python is cool", "JavaScript is amazing");
 
 for (const line of lines) {
     console.log(lines)
