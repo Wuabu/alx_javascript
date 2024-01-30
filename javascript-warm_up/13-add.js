7cc148351c126f13eda7b6b6a6d10af1186e15e8
Wuabu <wuabueric98@gmail>
2024-01-30 18:07:39 +0000

    new

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,26 +1,3 @@
-//add();
-//function add (num, num1){
-//    return num + num1;
-//} console.log(add(num,num1));
-
-// 13-add.js
-
-// Define and export the add function
-module.exports.add = function add(num1, num2) {
-    return num1 + num2;
-};
-
-
-// 13-main.js
-
-// Import the add function from 13-add.js
-// const add = require('./13-add').add;
-// const num1 = parseInt(prompt('Enter the first number: '), 10);
-// const num2 = parseInt(prompt('Enter the second number: '), 10);
-// // Call the add function with arguments 3 and 5 and print the result
-// console.log(add(num1, num2));
-
-
 function add(a, b) {
     return a + b;
   }
