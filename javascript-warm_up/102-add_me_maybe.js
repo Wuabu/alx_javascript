87d028e9fb92f9fa2d8766da825b934b856af5c1
Wuabu <wuabueric98@gmail>
2024-01-31 19:08:27 +0000

    add

--- javascript-warm_up/102-add_me_maybe.js
+++ javascript-warm_up/102-add_me_maybe.js
@@ -1,8 +1,8 @@
-// incrementAndCall.js
+// addMeMaybe.js
 
-// Define and export the incrementAndCall function
-module.exports.incrementAndCall = function incrementAndCall(number, theFunction) {
-    const incrementedNumber = number + 1;
-    theFunction(incrementedNumber);
+// Define and export the addMeMaybe function
+module.exports.addMeMaybe = function addMeMaybe(number, callback) {
+    const newValue = number + 1;
+    callback(newValue);
   };
   
