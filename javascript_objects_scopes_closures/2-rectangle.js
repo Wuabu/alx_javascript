c2fa809dae15416c81c99b13e4144fd443032f3a
Wuabu <wuabueric98@gmail>
2024-02-01 18:18:20 +0000

    remove

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -4,7 +4,7 @@ class Rectangle {
     constructor(w, h) {
         if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number') {
             // Create an empty object if w or h is equal to 0 or not a positive integer
-            return Object.create(null);
+            return {};
         }
 
         this.width = w;
