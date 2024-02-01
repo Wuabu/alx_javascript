2cf74c18555f527cbc7e59b2750c085d4c51a2fe
Wuabu <wuabueric98@gmail>
2024-02-01 15:19:08 +0000

    read

--- javascript_objects_scopes_closures/0-rectangle.js
+++ javascript_objects_scopes_closures/0-rectangle.js
@@ -1,3 +1,8 @@
 class Rectangle {
     // Class methods and properties can be added here if needed
   }
+  
+  // Example usage:
+  const myRectangle = new Rectangle();
+  console.log(myRectangle);
+  
