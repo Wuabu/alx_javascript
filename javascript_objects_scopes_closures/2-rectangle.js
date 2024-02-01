367d644f6c95d1cbfc3a22c027f3e339018d3581
Wuabu <wuabueric98@gmail>
2024-02-01 18:16:36 +0000

    object

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,47 +1,15 @@
 #!/usr/bin/node
-// class Rectangle {
-//     constructor (w, h) {
-//         if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number') {
-//             return {};
-//           }
-//         this.width = w;
-//         this.height = h;
-//     }
-
-// }
-
-// module.exports = Rectangle;
-
-// 2-rectangle.js
-
-// class Rectangle {
-//     constructor(w, h) {
-//       if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number') {
-//         // Create an empty object if w or h is equal to 0 or not a positive integer
-//         return {};
-//       }
-  
-//       this.width = w;
-//       this.height = h;
-//     }
-//   }
-  
-//   module.exports = Rectangle;
-
-// Rectangle.js
 
 class Rectangle {
     constructor(w, h) {
-      if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number') {
-        // Create an empty object if w or h is equal to 0 or not a positive integer
-        return Rectangle {};
-      }
-  
-      this.width = w;
-      this.height = h;
+        if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number') {
+            // Create an empty object if w or h is equal to 0 or not a positive integer
+            return Object.create(null);
+        }
+
+        this.width = w;
+        this.height = h;
     }
-  }
-  
-  module.exports = Rectangle;
-  
-  
+}
+
+module.exports = Rectangle;
