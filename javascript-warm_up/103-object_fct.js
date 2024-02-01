e97190cd4e63d46bbe88699b1212c059dc80568e
Wuabu <wuabueric98@gmail>
2024-02-01 17:24:59 +0000

    dem

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -1,28 +1,17 @@
-
-const { addMeMaybe } = require('./addMeMaybe');
-
-
-const incr = (number) => {
-  return number + 1;
+const myObject ={
+  type : "object",
+  value : 12
 };
+console.log(myObject);
 
-const myObject = {
-  type: 'object',
-  value: 12,
-};
-
-
-addMeMaybe(myObject.value, function (newValue) {
-  myObject.value = newValue;
-  myObject.incr = incr; 
-  console.log(myObject);
-});
-
-
-for (let i = 0; i < 3; i++) {
-  addMeMaybe(myObject.value, function (newValue) {
-    myObject.value = newValue;
-    myObject.incr = incr;
-    console.log(myObject);
-  });
+myObject.incr = function () {
+  this.value++;
 }
+
+
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
