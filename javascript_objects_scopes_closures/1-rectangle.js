151892ce535b8c283abc49d60c3abdb518d53de7
Wuabu <wuabueric98@gmail>
2024-02-01 17:50:38 +0000

    new

--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -1,8 +1,20 @@
-class rectangle {
-    constractor (w, h) {
-    this.width = w;
-    this.height = h
-    }
-}
+// class rectangle {
+//     constractor (w, h) {
+//     this.width = w;
+//     this.height = h
+//     }
+// }
 
-module.export = rectangle;
+// module.export = rectangle;
+
+// 1-rectangle.js
+
+class Rectangle {
+    constructor(w, h) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  
+  module.exports = Rectangle;
+  
