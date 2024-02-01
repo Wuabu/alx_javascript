9e4efed32edece1fe601d47d6d643fa0f4c98a9c
Wuabu <wuabueric98@gmail>
2024-02-01 18:24:58 +0000

    rect

---
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+class Rectangle {
+    constructor(w, h) {
+        if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number') {
+            // Create an empty object if w or h is equal to 0 or not a positive integer
+            return {};
+        }
+
+        this.width = w;
+        this.height = h;
+    }
+
+    print() {
+        for (let i = 0; i < this.height; i++) {
+            console.log('X'.repeat(this.width));
+        }
+    }
+}
+
+module.exports = Rectangle;
