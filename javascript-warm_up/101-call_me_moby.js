5b90c4fed0d61f0e79a8384121562de7936854e8
Wuabu <wuabueric98@gmail>
2024-02-01 17:16:42 +0000

    new

--- javascript-warm_up/101-call_me_moby.js
+++ javascript-warm_up/101-call_me_moby.js
@@ -2,8 +2,8 @@
 module.exports.executeXTimes = function executeXTimes(x, theFunction) {
   let count = x;  
   while (count > 0) {
-      theFunction();
-      count--;
-    }
-  };
+    theFunction();
+    count--;
+  }
+}
   
