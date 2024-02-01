8c2093ce6fb67e83fc0b124a228e21f245098b54
Wuabu <wuabueric98@gmail>
2024-02-01 18:06:01 +0000

    sss

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,30 +1,30 @@
 
-class Rectangle {
-    constructor (w, h) {
-        if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number') {
-            return {};
-          }
-        this.width = w;
-        this.height = h;
-    }
+// class Rectangle {
+//     constructor (w, h) {
+//         if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number') {
+//             return {};
+//           }
+//         this.width = w;
+//         this.height = h;
+//     }
 
-}
+// }
 
-module.exports = Rectangle;
+// module.exports = Rectangle;
 
 // 2-rectangle.js
 
-// class Rectangle {
-//     constructor(w, h) {
-//       if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number') {
-//         // Create an empty object if w or h is equal to 0 or not a positive integer
-//         return {};
-//       }
+class Rectangle {
+    constructor(w, h) {
+      if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number') {
+        // Create an empty object if w or h is equal to 0 or not a positive integer
+        return {};
+      }
   
-//       this.width = w;
-//       this.height = h;
-//     }
-//   }
+      this.width = w;
+      this.height = h;
+    }
+  }
   
 //   module.exports = Rectangle;
   
