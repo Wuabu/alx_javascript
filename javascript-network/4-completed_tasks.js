4bd9a99b8c293951dbd3f168e0d365f6961114e8
Wuabu <wuabueric98@gmail>
2024-02-08 18:11:03 +0000

    newnew

--- javascript-network/4-completed_tasks.js
+++ javascript-network/4-completed_tasks.js
@@ -1,36 +1,30 @@
 const request = require('request');
 
 // API URL for tasks
-const apiUrl = 'https://jsonplaceholder.typicode.com/tasks';
-
+const apiUrl = 'https://jsonplaceholder.typicode.com/todos';
 
 request(apiUrl, (error, response, body) => {
   if (error) {
     console.error('Error:', error);
     process.exit(1);
   }
-  if (response.statusCode === 200) {
-   
-    const tasks = JSON.parse(body);
 
+  if (response.statusCode === 200) {
+    const tasks = JSON.parse(body);
     const completedTasks = {};
-   
+
     tasks.forEach(task => {
       if (task.completed) {
-        if (completedTasks[task.userID]){
-          completedTasks[task.userID]++;
-        }
-        else {
-          completedTasks[task.userID] = 1;
+        if (completedTasks[task.userId]) {
+          completedTasks[task.userId]++;
+        } else {
+          completedTasks[task.userId] = 1;
         }
       }
-  //     if (task.completed) {
-  //       const userId = task.userId;
-  //       completedTasks[userId] = (completedTasks[userId] || 0) + 1;
-  //     } else {
-  //   console.error(`Error: Received status code ${response.statusCode} from the API.`);
-  // }
     });
+
     console.log(completedTasks);
-  } 
+  } else {
+    console.error(`Error: Received status code ${response.statusCode} from the API.`);
+  }
 });
